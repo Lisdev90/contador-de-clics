0899154f80b0db5aa42cc66bc9591c5e17f2be42
daviddevops96 <116809495+daviddevops96@users.noreply.github.com>
2023-03-10 09:20:20 +0100

    #2 #imagen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import freeCodeCampLogo
 
 function App() {
   return (
     <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo' src={}
+          alt='Logo de freeCodeCamp'/>
+      </div>
      
     </div>
   );
