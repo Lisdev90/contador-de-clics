a2b5f2add9ad13a46d73b37dfb321977ad28c723
daviddevops96 <116809495+daviddevops96@users.noreply.github.com>
2023-03-16 09:55:16 +0100

    #5 #estilobotonesClicyReiniciar

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,12 +1,15 @@
 import React from "react";
+import '../hojas-de-estilo/Boton.css'
 
 function Boton({ texto, esBotonDeClic, manejarClic}) {
     return (
         <button
-          className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+          className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
           onClick={manejarClic}>
           {texto}
         </button>
-    )
+    );
 
-}export default Boton;
+}
+
+export default Boton;
